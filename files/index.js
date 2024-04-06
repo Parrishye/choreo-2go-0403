e77e1bd2a807dde5e5dcb71d35ba554fec258901
Parrish <143268843+Parrishye@users.noreply.github.com>
2024-04-06 02:39:56 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -7,7 +7,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo-2go-0403.e5rga.link';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOTlmNjY1MTc3N2M5OTQxOTUwZDAzNzhhZWNkMTllYWYiLCJ0IjoiMmU2YTE4NDktNWI2ZS00NGQzLTk5YmYtODAyZTNlYjI3YzE3IiwicyI6Ik5UTTNOMk0wT1RFdE9EVTRNaTAwTlRreUxUbGlaVEF0TmpNMk5XTXhOamxtWmpVeiJ9';
+const ARGO_AUTH = process.env.ARGO_AUTH || 'cloudflared.exe service install eyJhIjoiOTlmNjY1MTc3N2M5OTQxOTUwZDAzNzhhZWNkMTllYWYiLCJ0IjoiMmU2YTE4NDktNWI2ZS00NGQzLTk5YmYtODAyZTNlYjI3YzE3IiwicyI6Ik16UTNObUZrTldZdE9EUmhOaTAwWmpFeUxUaGpPVFl0WlROaE56Vm1OREE1T0dJdyJ9';
 const CFIP = process.env.CFIP || 'government.se';
 const NAME = process.env.NAME || 'Choreo';
 
